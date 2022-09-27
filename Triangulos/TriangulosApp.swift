b638a6f5589280053327ff414f971f0077fecf3e
Yoltic2 <eyolticmg@icloud.com>
2022-09-26 22:21:15 -0500

    Initial Commit

---
+++ Triangulos/TriangulosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriangulosApp.swift
+//  Triangulos
+//
+//  Created by Emilio Y Martinez on 26/09/22.
+//
+
+import SwiftUI
+
+@main
+struct TriangulosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
