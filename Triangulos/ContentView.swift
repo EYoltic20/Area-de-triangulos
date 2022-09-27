f52d4cb55ab923c2facd3e6824ef560c5879cd2e
Yoltic2 <eyolticmg@icloud.com>
2022-09-27 17:50:38 -0500

    aplicacion terminada

--- Triangulos/ContentView.swift
+++ Triangulos/ContentView.swift
@@ -8,16 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+//    MARK: -Cambiamosel navigation title
+    init(){
+        UINavigationBar.appearance().titleTextAttributes = [.font:UIFont(name: "Georgia-Bold", size: 30)!]
+    }
     @State var a:String=""
     @State var b:String = ""
     @State var c: String = ""
     @State var valores:[String]=[""]
     @State var tipo_triangulo = ""
     @State var area = ""
+    @State var isPresented = false
     var body: some View {
         NavigationView{
-//            MARK: -customizando los textfields
-            VStack(alignment:.center,spacing: 50){
+            //            MARK: -customizando los textfields
+            VStack(alignment:.center,spacing: 40){
                 Spacer()
                 TextField("Ingrese el valor de a",text: $a)
                     .padding()
@@ -43,22 +48,40 @@ struct ContentView: View {
                     .font(.system(size: 20, weight: .medium, design: .monospaced))
                     .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue,lineWidth: 2))
                     .shadow(color: .black.opacity(0.2), radius: 3, x: 15, y: 17)
-                Spacer()
-//                MARK : -TIPO DE TRIANGULO
-                Text((tipo_triangulo == "") ? "" : "Tipo de triangulo:  \(tipo_triangulo)" )
-                    .padding()
-                    .font(.system(size: 20, weight:.medium, design: .monospaced))
-                Text((tipo_triangulo == "") ? "" : "Area del triangulo:  \(area)" )
-                    .padding()
-                    .font(.system(size: 20, weight:.medium, design: .monospaced))
-//                MARK : -GENERANDO EL TRIANGULO
-                Spacer()
-            
+                
+                //                MARK: -TIPO DE TRIANGULO
+                Group{
+                    Text((tipo_triangulo == "") ? "" : "Tipo de triangulo:" )
+                        
+                        .font(.system(size: 20, weight:.medium, design: .monospaced))
+                    Text((tipo_triangulo == "") ? "" : "\(tipo_triangulo)" )
+                        
+                        .font(.system(size: 20, weight:.medium, design: .monospaced))
+                    
+                    Text((tipo_triangulo == "") ? "" : "Area del triangulo:" )
+                        
+                        .font(.system(size: 20, weight:.medium, design: .monospaced))
+                    
+                    Text((tipo_triangulo == "") ? "" : "\(area)" )
+                        
+                        .font(.system(size: 20, weight:.medium, design: .monospaced))
+                }
+                //                MARK: -GENERANDO EL TRIANGULO
+                
+                
                 Button{
                     valores = calcular()
                     print(valores)
-                    tipo_triangulo=valores[0]
-                    area = valores[1]
+                    if(valores[0] == "x"){
+                        tipo_triangulo=""
+                        area = ""
+                        isPresented = true
+                    }
+                    else{
+                        tipo_triangulo=valores[0]
+                        area = valores[1]
+                        isPresented = false
+                    }
                 }label: {
                     HStack{
                         Image(systemName: "x.squareroot")
@@ -67,17 +90,29 @@ struct ContentView: View {
                         Text("Calcular")
                             .foregroundColor(.white)
                     }.frame(width:200,height: 50)
-                    .background(RoundedRectangle(cornerRadius: 30))
-
+                        .background(RoundedRectangle(cornerRadius: 30))
+                    
                 }
+                Spacer()
                 
             }.frame(maxWidth:.infinity,maxHeight: .infinity)
                 .padding()
-            .navigationTitle("Triangulos")
+                .navigationTitle("Triangulos")
+                .navigationBarTitleDisplayMode(.inline)
+                
+                
+                
+                
             
         }
+        .alert(isPresented:$isPresented){
+            Alert(title: Text("Valor o campo vacio"),
+                  message: Text("No deje ningun valor vacio"),
+                  dismissButton: .default(Text("OK"))
+            )
+        }
     }
-// MARK :- Conseguimos la funcion para calcular
+    // MARK: - Conseguimos la funcion para calcular
     func calcular()->[String]{
         if(a == "" || b == "" || c == "" ){
             return ["x","x"]
